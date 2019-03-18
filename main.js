5cc7d0164a303b884a7f98f1efc80d9b09a432d2
Gautam Worah <worah.gautam@gmail.com>
2019-03-17 23:32:27 -0400

    basic fuzz

--- main.js
+++ main.js
@@ -21,57 +21,57 @@ var data = fs.readFileSync(filename, 'utf8');
 var lines = data.split("\n");
 console.log(lines);
 
-for (var i=0;i<lines.length;i++){
-    prob=Math.random();
-    if (0<=prob<=0.2){
-        if(lines[i].match('while') || lines[i].match('for') || lines[i].match('if'))
-                {
+// for (var i=0;i<lines.length;i++){
+//     prob=Math.random();
+//     if (0<=prob<=0.2){
+//         if(lines[i].match('while') || lines[i].match('for') || lines[i].match('if'))
+//                 {
                     
-                    if(lines[i].match('<')){
-                    lines[i]=lines[i].replace('<', '>');
-                    console.log("replaced"+lines[i]);
-                    }
-                    else if(lines[i].match('>') && !lines[i].match('->')){
-                    lines[i]=lines[i].replace('>', '<');
-                    }
+//                     if(lines[i].match('<')){
+//                     lines[i]=lines[i].replace('<', '>');
+//                     console.log("replaced"+lines[i]);
+//                     }
+//                     else if(lines[i].match('>') && !lines[i].match('->')){
+//                     lines[i]=lines[i].replace('>', '<');
+//                     }
         
-                }
-                }
-    if (0.2<prob<=0.4){
-        if(lines[i].match('==')){
-            lines[i]=lines[i].replace(/==/g,'!=');}
-        else if(lines[i].match('!=')){
-            lines[i]=lines[i].replace(/!=/g,'==');}
-        }
-    if (0.4<prob<=0.6){
-        if((lines[i].match('while') || lines[i].match('for') || lines[i].match('if')) && lines[i].match(/[0]/))
-              {
-                lines[i]=lines[i].replace(/[0]/g,"1");
-              }
-              else if((lines[i].match('while') || lines[i].match('for') || lines[i].match('if')) && lines[i].match('1'))
-              { 
-                  lines[i]=lines[i].replace(/[1]/g,'0');
-              }
-    }
-    if (0.6<prob<=0.8){
-        var regMatch = lines[i].match('"(.*?)"');
-        if (regMatch!=null)
-                    {
-                        lines[i]=lines[i].replace(regMatch[1],mutateliteralstr(10));
-                    }
-    }
-    if (0.8<prob<=1.0){
-        if(lines[i].match('true')){
-            lines[i]=lines[i].replace('true', 'false');
-          }
-        else if(lines[i].match('false')){
+//                 }
+//                 }
+//     if (0.2<prob<=0.4){
+//         if(lines[i].match('==')){
+//             lines[i]=lines[i].replace(/==/g,'!=');}
+//         else if(lines[i].match('!=')){
+//             lines[i]=lines[i].replace(/!=/g,'==');}
+//         }
+//     if (0.4<prob<=0.6){
+//         if((lines[i].match('while') || lines[i].match('for') || lines[i].match('if')) && lines[i].match(/[0]/))
+//               {
+//                 lines[i]=lines[i].replace(/[0]/g,"1");
+//               }
+//               else if((lines[i].match('while') || lines[i].match('for') || lines[i].match('if')) && lines[i].match('1'))
+//               { 
+//                   lines[i]=lines[i].replace(/[1]/g,'0');
+//               }
+//     }
+//     if (0.6<prob<=0.8){
+//         var regMatch = lines[i].match('"(.*?)"');
+//         if (regMatch!=null)
+//                     {
+//                         lines[i]=lines[i].replace(regMatch[1],mutateliteralstr(10));
+//                     }
+//     }
+//     if (0.8<prob<=1.0){
+//         if(lines[i].match('true')){
+//             lines[i]=lines[i].replace('true', 'false');
+//           }
+//         else if(lines[i].match('false')){
             
-              lines[i]=lines[i].replace('false', 'true');
-            }
+//               lines[i]=lines[i].replace('false', 'true');
+//             }
         
-    }
+//     }
 
-}
+// }
 
 function mutateliteralstr(length)
 {
